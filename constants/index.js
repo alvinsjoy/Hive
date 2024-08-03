99e466318c61ed60eba3304eaf27294a23f361fd
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-08-03 14:59:20 +0530

    refractor colors and add community page

--- constants/index.js
+++ constants/index.js
@@ -32,13 +32,13 @@ export const sidebarLinks = [
 ];
 
 export const profileTabs = [
-  { value: 'buzzes', label: 'Buzzes', icon: '/assets/reply.svg' },
+  { value: 'threads', label: 'Buzzes', icon: '/assets/reply.svg' },
   { value: 'replies', label: 'Replies', icon: '/assets/members.svg' },
   { value: 'tagged', label: 'Tagged', icon: '/assets/tag.svg' },
 ];
 
 export const communityTabs = [
-  { value: 'buzzes', label: 'Buzzes', icon: '/assets/reply.svg' },
+  { value: 'threads', label: 'Buzzes', icon: '/assets/reply.svg' },
   { value: 'members', label: 'Members', icon: '/assets/members.svg' },
   { value: 'requests', label: 'Requests', icon: '/assets/request.svg' },
 ];
