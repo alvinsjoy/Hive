972f7929806e96cfcc9735947f76d92a94812f1b
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-08-03 11:42:55 +0530

    add major changes

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,43 @@
+import { ClerkProvider } from '@clerk/nextjs';
+import { Inter } from 'next/font/google';
+import type { Metadata } from 'next';
+import { dark } from '@clerk/themes';
+
+import '../globals.css';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import Bottombar from '@/components/shared/Bottombar';
+import RightSidebar from '@/components/shared/RightSidebar';
+import Topbar from '@/components/shared/Topbar';
+
+export const metadata = {
+  title: 'Hive',
+  description: 'Hive app',
+};
+const inter = Inter({ subsets: ['latin'] });
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en">
+        <body className={inter.className}>
+          <Topbar />
+          <main className="flex flex-row">
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
