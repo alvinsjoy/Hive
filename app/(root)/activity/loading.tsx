3eddccdcba5359d3ed1d1e14c80e34e091155ded
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-08-17 20:09:12 +0530

    refactor loading component to display 5 activity cards

--- app/(root)/activity/loading.tsx
+++ app/(root)/activity/loading.tsx
@@ -6,7 +6,7 @@ function Loading() {
       <h1 className="head-text">Activity</h1>
 
       <section className="mt-10 flex flex-col gap-5">
-        {Array.from({ length: 10 }).map((_, i) => (
+        {Array.from({ length: 5 }).map((_, i) => (
           <article key={i} className="activity-card">
             <Skeleton className="relative h-5 w-5 rounded-full" />
             <Skeleton className="h-4 w-[250px]" />
