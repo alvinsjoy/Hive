f1090d821a50202cb7e13caa71ef5e70aef5a983
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-09-04 20:59:25 +0530

    Refactor loading components for profile and edit pages

--- app/(root)/profile/edit/loading.tsx
+++ app/(root)/profile/edit/loading.tsx
@@ -2,31 +2,30 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 function Loading() {
   return (
-    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+    <main className="mx-auto max-w-3xl px-10 py-20">
       <Skeleton className="h-8 w-40" />
       <Skeleton className="mt-3 h-5 w-72" />
 
       <section className="mt-9 bg-dark-2 p-10">
-        <div className="flex flex-col justify-start gap-10">
+        <div className="flex flex-col gap-10">
           <div className="flex items-center gap-4">
             <Skeleton className="h-24 w-24 rounded-full" />
             <div className="flex-1">
-              <Skeleton className="h-6 w-40" />
-              <Skeleton className="mt-2 h-4 w-20" />
+              <Skeleton className="h-6 w-40 max-sm:w-28" />
             </div>
           </div>
 
-          <div className="flex w-full flex-col gap-3">
+          <div className="flex flex-col gap-3">
             <Skeleton className="h-5 w-20" />
             <Skeleton className="h-10 w-full" />
           </div>
 
-          <div className="flex w-full flex-col gap-3">
+          <div className="flex flex-col gap-3">
             <Skeleton className="h-5 w-20" />
             <Skeleton className="h-10 w-full" />
           </div>
 
-          <div className="flex w-full flex-col gap-3">
+          <div className="flex flex-col gap-3">
             <Skeleton className="h-5 w-20" />
             <Skeleton className="h-32 w-full" />
           </div>
